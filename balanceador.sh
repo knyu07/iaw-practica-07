9fa8733678e0cf4883a2f9d9bb4526683ed7a3e6
knyu07 <pventura9839@gmail.com>
2020-11-26 18:02:06 +0100

    terminada

--- balanceador.sh
+++ balanceador.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #VARIABLES
-IP_FRONTEND_1=52.201.221.167
-IP_FRONTEND_2=52.201.212.21
+IP_FRONTEND_1=34.238.85.119
+IP_FRONTEND_2=54.196.137.100 
 
 # Actualizacion de los repositorios de la maquina.
 apt update
@@ -15,6 +15,9 @@ cd /home/ubuntu
 git clone https://github.com/knyu07/iaw-practica-07
 mv iaw-practica-07/default /etc/nginx/sites-available
 rm -r /home/ubuntu/iaw-practica-07
+
+#Cambiamos los permisos
+chown www-data:www-data * -R
 
 #Asignamos variables
 sed -i "s/IP_FRONTEND_1/$IP_FRONTEND_1/" /etc/nginx/sites-available/default
